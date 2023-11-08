41de89b1990b2b8dc363670ba3a4e7062e4a7925
sftm0715 <sftm0715@gmail.com>
2023-11-08 23:24:34 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/jwtLoginAndOauth2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jwtLoginAndOauth2 {
+    public static void main(String[] args) {
+        SpringApplication.run(jwtLoginAndOauth2.class, args);
+    }
+
+}
